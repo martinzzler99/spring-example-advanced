6ec47a7729973212f0688dfa7464e47c061c71d6
martinzzler99 <89845738+martinzzler99@users.noreply.github.com>
2021-09-21 13:21:21 +0200

    asdf
    asdf

---
+++ src/main/java/com/example/service/FruitController.java
@@ -0,0 +1,38 @@
+package com.example.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+// Add trace dependecny here
+
+@Controller
+@RequestMapping("/fruits")
+public class FruitController {
+
+    private List<Fruit> fruits = new ArrayList<>();
+
+    // Add tracer here
+
+
+    @GetMapping
+    public String home(Model model) {
+
+        // TODO: Add tracing here
+
+        model.addAttribute("fruits", fruits);     // For the List view
+        model.addAttribute("fruitForm", new Fruit()); // For the Form
+        return "home";
+    }
+
+    @PostMapping
+    public String createFruit(@ModelAttribute Fruit fruit) {
+        fruits.add(fruit);
+        return "redirect:/fruits";
+    }
+}
